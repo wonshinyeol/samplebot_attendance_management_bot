9d4c9da919eaa6b9996dae67e6378e09a9d3bc1d
wonshinyeol <60927503+wonshinyeol@users.noreply.github.com>
2020-02-11 22:26:32 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -26,14 +26,14 @@ TZone = "Asia/Tokyo"
 
 # account
 ADMIN_ACCOUNT = "admin@xxx.com"
-DOMAIN_ID = 0
+DOMAIN_ID = 156452
 
 # api
-API_ID = "kr1EHxxxxfJVz"
-SERVER_CONSUMER_KEY = "To8SnC7sxxxxxjqXZhO"
+API_ID = "kr1OJsUnErhWM"
+SERVER_CONSUMER_KEY = "9bV8RlJI9U2RXqeiiY0H"
 
-SERVER_ID = "96460cc1e778402xxxxxfe35fa97ce76"
-SECRET_KEY_NAME = "private_201xxxxx164308.key"
+SERVER_ID = "cd057b41de414a74892c2e206ee20a34"
+SECRET_KEY_NAME = "private_20200211222456.key"
 
 # LOCAL ADDRESS
 LOCAL_ADDRESS = "https://[Heroku app name].herokuapp.com/"
